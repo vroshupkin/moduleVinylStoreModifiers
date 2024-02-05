adb0e1c9b5608c55cd02f575e162aea1eb6752f0
Vadim Roschupkin <vroshupkin1@gmail.com>
2024-02-05 22:09:47 +0300

    Рефакторинг

--- AutoColumn.ts
+++ AutoColumn.ts
@@ -1,121 +1,5 @@
 namespace AutoColumn
 {   
-    type Range = GoogleAppsScript.Spreadsheet.Range;
-    
-
-// export function CellInRange(x: number, y: number, range: Range)
-// {
-//   return !(
-//     x < range.getColumn() ||
-//     x > range.getColumn() + range.getWidth() - 1 || 
-//     y < range.getRow() ||
-//     y > range.getRow() + range.getHeight() - 1 
-//   );
-// }
-
- interface AutoColumnFuncParam {
-    sheet: GoogleAppsScript.Spreadsheet.Sheet,
-    /** Координаты заголовка с записью 'auto' */
-    header_coords: {y: number, x: number},
-    /** Координаты начало значений с которых начинается автозаполнение */
-    values_coords: {y: number, x: number},
-    /** size: columns - ширина таблица, в который ищется 'auto'. rows - глубина, на которой надо протянуть */
-    size: {rows: number, columns: number}
-  }
-
-export function AutoColumnFunc(params: AutoColumnFuncParam, get_cell_type?: typeof AutoColumn.get_cell_type)
-{
-  const { sheet, values_coords, header_coords, size } = params;
-  const header_range = sheet.getRange(header_coords.y, header_coords.x, 1, size.columns);
-  const notes = header_range.getNotes()[0];
-  
-  if(!Array.isArray(notes)) { return; }
-
-  const rows = size.rows - values_coords.y + 1;
-  for (const [ i, note ] of notes.entries()) 
-  { 
-    if(i === 0)
-    {
-      continue;   
-    }
-
-    const x = values_coords.x + i;
-    const note_type = (note + '').trim().toLowerCase();
-    
-    
-    if(note_type.length > 4)
-    {
-      Logger.log(note_type);
-    }
-
-    if(note_type.includes('discogsFormat'.toLowerCase()))
-    {
-      const fillFormat = () => 
-      {
-        const param = note_type.slice(
-          note_type.indexOf('(')+ 1,
-          note_type.indexOf(')')
-        );
-      
-        const source_x = Tools.calcXPosition(param);
-
-        const insert_column = sheet.getRange(values_coords.y, x, size.rows);
-        const source_column = sheet.getRange(values_coords.y, source_x, size.rows);
-
-        Tools.fillTableByTable(insert_column, source_column, Discogs.formatHandle);
-      };
-
-      Tools.timestampDecorator(fillFormat, 'fillFormat')();
-      
-    }
-
-    if(note_type !== 'auto')
-    {
-      continue;
-    }
-
-    const cell_type = get_cell_type?
-      get_cell_type(values_coords.y, x, sheet): Constants.EnumCellType.DEFAULT;
-    
-    if(cell_type === Constants.EnumCellType.DEFAULT)
-    {
-      const pattern_cell = sheet.getRange(values_coords.y, x);
-      const destination = sheet.getRange(values_coords.y, x, rows);
-      pattern_cell.autoFill(destination, SpreadsheetApp.AutoFillSeries.DEFAULT_SERIES); 
-      
-    }
-    else if(cell_type === Constants.EnumCellType.CHECKBOX)
-    {
-      // Logger.log('Constants.EnumCellType.CHECKBOX');
-      // !!! WARNING size_rows должен называется insert_y
-      const insert_cell = sheet.getRange(size.rows, x);
-      const check_box_data_validation = SpreadsheetApp.newDataValidation().requireCheckbox().build();
-      
-      insert_cell.setDataValidation(check_box_data_validation);
-
-      insert_cell.setValue(false);
-      
-    }
-    else if(cell_type === Constants.EnumCellType.DROPDOWN)
-    {
-      
-      // Logger.log('Constants.EnumCellType.DROPDOWN');
-      const pattern_cell = sheet.getRange(values_coords.y, x);
-      // Logger.log(pattern_cell.getValue());
-      // const data_validation = pattern_cell.getDataValidation();
-      // Logger.log(data_validation + '');
-
-      const destination = sheet.getRange(size.rows, x);
-      pattern_cell.copyTo(destination);
-
-      // destination.setDataValidation(data_validation);
-      // destination.setValue(pattern_cell.getValue());
-
-    }
-  }
-}
-
-
   /**
    * @param values_range - Ячейки откуда протягивается автозначения
    * Сохраняет в кеш по ключу `cell_types_header in ${sheet_name}` строку вида '0011212'. 
@@ -132,7 +16,6 @@ export function AutoColumnFunc(params: AutoColumnFuncParam, get_cell_type?: type
     let cache_str = '';
     while(columns)
     {
-
       x++;
       cache_str +=  AutoColumn.get_cell_type(y, x, sheet);
       columns--;
@@ -163,30 +46,64 @@ export function AutoColumnFunc(params: AutoColumnFuncParam, get_cell_type?: type
   };
   
   /**
-     * Класс хранящий данные для других классов
-     */
-    export class DataSource
-    {
-      
+  * Класс хранящий данные для других классов
+  */
+    export class DataSourceWithCache
+    { 
+      cache_values = '';
+
       constructor(
         public header_coords: {y: number, x: number},
         public values_coords: {y: number, x: number},
-        public sheet: GoogleAppsScript.Spreadsheet.Sheet
-      )
-      {        
-      }
+        public sheet: GoogleAppsScript.Spreadsheet.Sheet,
+      ){}
 
+      /** Количество колонок */
       get columns()
       {
         return this.sheet.getLastColumn();
       }
 
-      get values_range()
+      get values_row()
       {
         const { y, x } = this.values_coords;
         
         return this.sheet.getRange(y, x, 1, this.columns);
       }
+
+      get header_row()
+      {
+        const { y, x } = this.header_coords;
+        
+        return this.sheet.getRange(y, x, 1, this.columns);
+      }
+      
+      
+      /**
+       * @param x номер координаты
+       * Остальные параметры нужны как адаптеры
+       */
+      getCellType = (x: number) => 
+      {
+        const start_x = this.values_coords.x;
+        
+        return Number(this.cache_values[x - start_x - 1]) as Constants.EnumCellType;
+      };
+
+      /**
+       * Используется для обновление кеша когда открывается таблица
+       */
+      updateCellTypesHeader = () => 
+      {
+        AutoColumn.updateCellTypeHeaderCache(
+          this.values_coords,
+          this.sheet.getLastColumn(),
+          this.sheet
+        );
+
+        const cache = CacheService.getScriptCache();
+        this.cache_values = cache.get(`cell_types_header: ${this.sheet.getSheetName()}`) ?? '';
+      };
     }
 
     /**
@@ -194,71 +111,115 @@ export function AutoColumnFunc(params: AutoColumnFuncParam, get_cell_type?: type
      */
     export class AutoColumnMechanism
     {
-      cache_values = '';
-
-      constructor(private data_source: DataSource)
+      constructor(private data_source: DataSourceWithCache)
       {
-        this.updateCellTypesHeader();
+        this.data_source.updateCellTypesHeader();
       }
       
-
-      /**
-       * @param x номер координаты
-       * Остальные параметры нужны как адаптеры
-       */
-      getCellType = (y: number, x: number, sheet: GoogleAppsScript.Spreadsheet.Sheet ) => 
-      {
-        const start_x = this.data_source.values_coords.x;
-        
-        // Logger.log(this.cache_values[x - start_x]);
-        // Logger.log(JSON.stringify(
-        //   {
-        //     x: x,
-        //     x_start_x: x - start_x,
-        //     value: this.cache_values[x - start_x],
-        //     this_cache_values: this.cache_values
-        //   }
-        // ));
-        
-        return Number(this.cache_values[x - start_x - 1]) as Constants.EnumCellType;
-      };
-
-      /**
-       * Используется для обновление кеша когда открывается таблица 
-       * TODO и когда меняются аннотации
-       */
-      updateCellTypesHeader = () => 
-      {
-        AutoColumn.updateCellTypeHeaderCache(
-          this.data_source.values_coords,
-          this.data_source.sheet.getLastColumn(),
-          this.data_source.sheet
-        );
-
-        const cache = CacheService.getScriptCache();
-        const sheet = this.data_source.sheet;
-
-        this.cache_values = cache.get(`cell_types_header: ${sheet.getSheetName()}`) ?? '';
-      };
-      
-
       /**
        * @param y - номер строки для автозаполнения
        */
       do = (y: number) => 
       {
-        AutoColumn.AutoColumnFunc({ 
-          sheet: this.data_source.sheet,
-          size: { 
-            columns: this.data_source.columns,
-            rows: y
-          },
-          header_coords: this.data_source.header_coords,
-          values_coords: this.data_source.values_coords,
-        }, this.getCellType);
+        const { header_row: header_range, values_coords } = this.data_source;
+
+        const notes = header_range.getNotes()[0];
+        if(!Array.isArray(notes)) { return; }
+
+        // const rows = y - values_coords.y + 1;
+
+        for (const [ i, note ] of notes.entries()) 
+        { 
+          if(i === 0)
+          {
+            continue;   
+          }
+
+          const x = values_coords.x + i;
+          const note_type = (note + '').trim();
+          
+
+          if(note_type.includes('DiscogsFormat'))
+          {
+            Tools.timestampDecorator(this.discogsFormatAuto, 'discogsFormat')(y, x, this.getParam(note));
+          }
+          else if(note_type.toLowerCase().includes('auto'))
+          {
+            this.autoFunction(x, y);
+          }
+          else if(note_type.includes('SetValues'))
+          {
+            this.setValues(y, x, this.getParam(note));
+          }
+        }
+      };
+
+      private autoFunction = (y: number, x: number) => 
+      {
+        const { getCellType, sheet, values_coords } = this.data_source;
+        const rows = y - values_coords.y + 1;
+
+        const cell_type = getCellType(x);
+    
+        if(cell_type === Constants.EnumCellType.DEFAULT)
+        {
+          const pattern_cell = sheet.getRange(values_coords.y, x);
+          const insert_column = sheet.getRange(values_coords.y, x, rows);
+          
+          pattern_cell.autoFill(insert_column, SpreadsheetApp.AutoFillSeries.DEFAULT_SERIES); 
+      
+        }
+        else if(cell_type === Constants.EnumCellType.CHECKBOX)
+        {
+          // Logger.log('Constants.EnumCellType.CHECKBOX');
+          const insert_cell = sheet.getRange(y, x);
+          const check_box_data_validation = SpreadsheetApp.newDataValidation().requireCheckbox().build();
+      
+          insert_cell.setDataValidation(check_box_data_validation);
+          insert_cell.setValue(false);
+        }
+        else if(cell_type === Constants.EnumCellType.DROPDOWN)
+        { 
+          const insert_cell = sheet.getRange(y, x);         
+          const pattern_cell = sheet.getRange(values_coords.y, x); 
+
+          pattern_cell.copyTo(insert_cell);
+        }
+      };
+
+      private discogsFormatAuto = (y: number, x: number, param: string) =>
+      {
+        this.autoColumnFabricFunction(y, x, param, Discogs.formatHandle);
+      };
+
+      private setValues = (y: number, x: number, param: string) => 
+      {
+        this.autoColumnFabricFunction(y, x, param, (a: string) => a);
+      };
+
+      private autoColumnFabricFunction = (y: number, x: number, param: string, fn: (val: any) => any) =>
+      {
+        const { sheet, values_coords } = this.data_source;
+        
+        const source_x = Tools.calcXPosition(param);
+
+        const insert_column = sheet.getRange(values_coords.y, x, y);
+        const source_column = sheet.getRange(values_coords.y, source_x, y);
+
+        Tools.fillTableByTable(insert_column, source_column, fn);
+      };
+    
+      /**
+       * @example "DiscogsFormat(K)" => "K"
+       */
+      private getParam = (str: string) =>
+      {
+        return str.slice(
+          str.indexOf('(')+ 1,
+          str.indexOf(')')
+        );
       };
     }
-
 }
 
 
